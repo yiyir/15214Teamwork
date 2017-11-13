8f93d783d88b1be85fa497fa66e08a67b5d79f14
Yiyi Ren <yiyiren@Yiyis-MacBook-Pro.local>
2017-11-13 16:41:31 -0500

    third

--- plugins/src/main/java/edu/cmu/cs/cs214/hw5/plugin/DataPlugin.java
+++ plugins/src/main/java/edu/cmu/cs/cs214/hw5/plugin/DataPlugin.java
@@ -5,7 +5,5 @@ import edu.cmu.cs.cs214.hw5.framework.core.Restaurant;
 import java.util.List;
 
 public interface DataPlugin {
-    void retrieveData(String city);
-    List<Restaurant> getRestaurants();
-    String getName();
+    List<Restaurant> getRestaurants(String city);
 }
